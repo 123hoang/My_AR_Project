563f8f10bf998790e63f1ba13bef18172038105e
Bùi Khánh Hoàng <93996538+123hoang@users.noreply.github.com>
2024-04-19 07:42:33 +0700

    Add files via upload

---
+++ my_first_project_1904.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+#Read Image from file load from directory
+img1 = cv.imread("image/saigon.jpg")
+#Size height, wweight
+print("The characteristic of "+"image1 is ")
+print(img1.shape)
+#Size of memory
+print("The size of the img1 is")
+print(img1.size)
+
+##Second image
+img2 = cv.imread("/image/Van-gogh.jpg")
+#Size height, wweight
+print("The characteristic of "+"image2 is ")
+print(img2.shape)
+#Size of memory
+print("The size of the img2 is")
+print(img2.size)
