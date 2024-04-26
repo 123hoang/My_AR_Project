b8b35e0dcd4e30844e2464d6c07ee69c47286523
Bùi Khánh Hoàng <93996538+123hoang@users.noreply.github.com>
2024-04-26 20:46:30 +0700

    Add files via upload

--- my_first_project_1904.py
+++ my_first_project_1904.py
@@ -9,11 +9,11 @@ print(img1.shape)
 print("The size of the img1 is")
 print(img1.size)
 
-##Second image
-img2 = cv.imread("/image/Van-gogh.jpg")
+#Read Image from file load from directory
+img2 = cv.imread("image/Van-gogh.jpg")
 #Size height, wweight
 print("The characteristic of "+"image2 is ")
 print(img2.shape)
 #Size of memory
-print("The size of the img2 is")
+print("The size of the img1 is")
 print(img2.size)
